aa7b4960b457f806c03dabd528b1af616adf0638
yym-yumeng123 <18026493873@163.com>
2022-07-28 19:42:09 +0800

    创建路由

--- app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/users", to: "users#create"
-  get "/users/:id", to: "users#show"
+  namespace :api do
+    namespace :v1 do
+      resources :validation_code, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 end
