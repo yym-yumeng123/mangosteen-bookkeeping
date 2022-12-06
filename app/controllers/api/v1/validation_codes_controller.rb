2b1ec0b799e677e53b40fcbcb0b86ab169ffb60a
yym-yumeng123 <18026493873@163.com>
2022-12-06 18:10:26 +0800

    test validation_codes

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    head 422
+    code = SecureRandom.random_number.to_s[2..7]
+    validation_code = ValidationCode.new email: params[:email], code: code, kind: 'sign_in'
+    if validation_code.save
+      # render json: { resouce: validation_code }, status: :created
+      head 200
+    else
+      render json: { errors: validation_code.errors }, status: :unprocessable_entity
+    end
   end
 end
