78cd05c9190fb038cd2aab7443068275401a86c9
yym-yumeng123 <18026493873@163.com>
2022-12-23 20:57:09 +0800

    user method header

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -5,8 +5,6 @@ class AutoJwt
 
   def call(env)
     header = env["HTTP_AUTHORIZATION"]
-    p 'header'
-    p header
     jwt = header.split(" ")[1] rescue ""
     payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: "HS256" } rescue nil
     env["current_user_id"] = payload[0]["user_id"] rescue nil
