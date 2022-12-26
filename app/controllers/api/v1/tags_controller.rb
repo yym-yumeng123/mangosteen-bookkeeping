19df7bb033c8dc18f8a47e137424b72e8b2df706
yym-yumeng123 <18026493873@163.com>
2022-12-26 21:36:01 +0800

    完成创建标签API

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TagsController < ApplicationController
   def index
     current_user = User.find request.env['current_user_id']
-    return render status: :not_found if current_user.nil?  
+    return render status: 401 if current_user.nil?  
     tags = Tag.where(user_id: current_user.id).page(params[:page])
     render json: {resources: tags, pager: {
       page: params[:page] || 1,
@@ -10,4 +10,15 @@ class Api::V1::TagsController < ApplicationController
     }}
   end
   
+  def create
+    current_user = User.find request.env['current_user_id']
+    return render status: 401 if current_user.nil?  
+
+    tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user.id
+    if tag.save
+      render json: {resource: tag}, status: :ok
+    else
+      render json: {errors: tag.errors}, status: :unprocessable_entity
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :sign, presence: true
 end
