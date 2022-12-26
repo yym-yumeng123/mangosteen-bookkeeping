61efa177c2bc1c9e9877b17b0a88773eaa8a1abc
yym-yumeng123 <18026493873@163.com>
2022-12-26 23:22:09 +0800

    item 接口

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::ItemsController < ApplicationController
   def index
     current_user_id = request.env["current_user_id"]
     return head :unauthorized if current_user_id.nil?
-
-    items = Item.where({ user_id: current_user_id }).where({ created_at: params[:created_after]..params[:created_before] }).page(params[:page])
-
+    items = Item.where({ user_id: current_user_id })
+      .where({ created_at: params[:created_after]..params[:created_before] })
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
@@ -13,8 +13,8 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new params.permit(:amount, :tags_id, :happen_at)  
-    item.user_id = request.env['current_user_id']
+    item = Item.new params.permit(:amount, :happend_at, tags_id: [])
+    item.user_id = request.env["current_user_id"]
     if item.save
       render json: { resource: item }
     else
